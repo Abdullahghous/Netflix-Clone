e61d534bf6cc12920ef69fd80c5cda6856b5db15
AbdullahGhous <muhammadabdullahghous@gmail.com>
2020-10-26 21:52:43 +0500

    pushing code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import request from './request';
+import Banner from './Banner';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+     <Banner/>
+     <Row title="NETFLIX ORIGINALS" fetchURL={request.fetchNetflixOriginals} isLargeRow />
+     <Row title="TOP TRENDING" fetchURL={request.fetchTrending} />
+     <Row title="TOP RATED" fetchURL={request.fetchTopRated} />
+     <Row title="ACTION MOVIES" fetchURL={request.fetchActionMovies} />
+     <Row title="COMEDY MOVIES" fetchURL={request.fetchComedyMovies} />
+     <Row title="HORROR MOVIES" fetchURL={request.fetchHorrorMovies} />
+     <Row title="ROMANCE MOVIES" fetchURL={request.fetchRomanceMovies} />
+     <Row title="DOUCMENTARIES" fetchURL={request.fetchDocumentaries} />
     </div>
   );
 }
